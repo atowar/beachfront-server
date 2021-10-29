dd6420ed4ba7a736065eda6f412c7662ec802b38
Atowar Rahman <ahmed.anik76@gmail.com>
2021-10-29 22:16:57 +0600

    uploaded

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
+
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbo7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
+async function run () {
+    try{
+        //database connection
+        await client.connect();
+        const database = client.db('beachfront');
+        const serviceCollection = database.collection('services');
+
+        //get services API
+
+        app.get('/services', async(req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('BeachFront server running')
+})
+
+app.listen(port, ()=>{
+    console.log('PORT: ', port);
+})
+
